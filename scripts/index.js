51427360fc0c89726dcd84fe6d36ed7843b1a517
Lukas Volgger <55922592+LukasVolgger@users.noreply.github.com>
2022-04-06 06:28:57 +0200

    create addTask() and object structure

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,26 @@
 
 // ####################################### GLOBAL SCOPE #######################################
 
+// Stores all task objects
+tasks = [];
 
 // ####################################### MAIN FUNCTIONS #######################################
 
 function init() {
     includeHTML();
+}
+
+/**
+ * Gets the user input and creates an object 
+ */
+function addTask() {
+    // Basic object structure
+    // let task = {
+    //     'title': '',
+    //     'category': '',
+    //     'description': '',
+    //     'due_date': '',
+    //     'urgency': '',
+    //     'assigned_to': ''
+    // };
 }
