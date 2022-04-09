d63ed896ad332fd3c722d5cbd04edd509f88dedd
samuelbergen <kontakt@samuel-bergen.de>
2022-04-09 11:06:45 +0200

    add showBacklog()-Function

--- scripts/backlog.js
+++ scripts/backlog.js
@@ -6,6 +6,16 @@
 let tasks = [];
 setURL('http://gruppe-211.developerakademie.net/smallest_backend_ever');
 
+let task = [{
+    'assigned_to': "samuel_bergen",
+    'category': "category_2",
+    'creation_date': 1649493181425,
+    'description': "User-Login erstellen mit Nutzername und Passwort\nUser anlegen",
+    'due_date': "2022-04-11",
+    'title': "User-Login machen",
+    'urgency': "high"
+}];
+
 
 // ####################################### MAIN FUNCTIONS #######################################
 
@@ -13,14 +23,16 @@ async function init() {
     includeHTML();
     await downloadFromServer();
     await loadFromBackend();
+    showBacklog();
 }
 
 /**
  * Show all unallocated Objects from Server
  */
 function showBacklog() {
-    loadFromBackend();
-    
+    let content = document.getElementById('backlog-content');
+    content.innerHTML = '';
+    createBacklogItem();
 }
 
 /**
@@ -29,4 +41,51 @@ function showBacklog() {
 function loadFromBackend() {
     let tasksAsJSON = backend.getItem('tasks');
     tasks = JSON.parse(tasksAsJSON) || [];
+}
+
+
+/**
+ * Create a Backlog-item
+ */
+function createBacklogItem() {
+    let content = document.getElementById('backlog-content');
+    for (let i = 0; i < tasks.length; i++) {
+        content.innerHTML += templateBacklogItem(i, tasks[i]);
+    }
+}
+
+
+/**
+ * This function allows you to edit a backlog-item after create
+ */
+function editTask(i) {
+
+}
+
+/**
+ * This function generate HTML-Code for one Backlog-Item
+ */
+function templateBacklogItem(i, task) {
+    return `
+        <div class="backlog-item ${task.category}" id="backlog-item-${i}" onclick="editTask(${i})">
+            <div class="person">
+                <img class="rounded-circle profile-picture" src="imgs/pp_${task.assigned_to}.jfif" alt="">
+                <div class="person-name">
+                    <span>${task.assigned_to}</span>
+                    <a href="mailto:${task.assigned_to}@join.com">${task.assigned_to}@join.com</a>
+                </div>
+            </div>
+            <div> ${task.category} </div>
+            <div>${task.description}</div>
+        </div>
+    `;
+}
+
+/**
+ * This function generate dynamic HTML-Code for the edit-Dialog
+ */
+function templateEditTask(i, task) {
+    return `
+    
+    `;
 }
