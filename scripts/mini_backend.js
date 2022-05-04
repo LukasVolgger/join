2517220e9916ea3988366e5a2c76e485402ecc0d
samuelbergen <kontakt@samuel-bergen.de>
2022-05-04 17:57:47 +0200

    JS-Doc mini_backend.js

--- scripts/mini_backend.js
+++ scripts/mini_backend.js
@@ -1,3 +1,6 @@
+// ####################################### GLOBAL SCOPE #######################################
+
+
 let jsonFromServer = {};
 let BASE_SERVER_URL;
 
@@ -17,60 +20,46 @@ const backend = {
         return saveJSONToServer();
     }
 };
+
+
+// ####################################### MAIN FUNCTIONS #######################################
+
+
+// Load the function downloadFromServer() when loading a window
 window.onload = async function() {
     downloadFromServer();
 }
 
+
+/**
+ * Download the saved String from Server and parse it
+ */
 async function downloadFromServer() {
     let result = await loadJSONFromServer();
     jsonFromServer = JSON.parse(result);
     // console.log('Loaded', result);
 }
 
+
+/**
+ * Sets the url from the server
+ * 
+ * @param {string} url 
+ */
 function setURL(url) {
     BASE_SERVER_URL = url;
 }
+
 
 /**
  * Loads a JSON or JSON Array to the Server
  * payload {JSON | Array} - The payload you want to store
  */
-
 async function loadJSONFromServer() {
     let response = await fetch(BASE_SERVER_URL + '/nocors.php?json=database&noache=' + (new Date().getTime()));
     return await response.text();
 
 }
-
-function loadJSONFromServerOld() {
-    return new Promise(function(resolve, reject) {
-        let xhttp = new XMLHttpRequest();
-        let proxy = determineProxySettings();
-        let serverURL = proxy + BASE_SERVER_URL + '/nocors.php?json=database&noache=' + (new Date().getTime());
-
-
-
-
-        xhttp.open('GET', serverURL);
-
-        xhttp.onreadystatechange = function(oEvent) {
-            if (xhttp.readyState === 4) {
-                if (xhttp.status >= 200 && xhttp.status <= 399) {
-                    resolve(xhttp.responseText);
-                } else {
-                    reject(xhttp.statusText);
-                }
-            }
-        };
-
-        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        xhttp.send();
-
-    });
-}
-
-
-
 
 
 /**
@@ -100,6 +89,11 @@ function saveJSONToServer() {
 }
 
 
+/**
+ * Determine the Proxy Settings
+ * 
+ * @returns - returns an url under certain conditions
+ */
 function determineProxySettings() {
     return '';
 
